0f1f49aae4d97870bf886187c716ec948a743feb
woaimi520 <woaimi520@163.com>
2020-02-10 21:28:43 +0800

    基本框架

--- app/src/main/java/com/renyu/administrator/Cache/MemoryCacheObservableextends.java
+++ app/src/main/java/com/renyu/administrator/Cache/MemoryCacheObservableextends.java
@@ -1,5 +1,10 @@
 package com.renyu.administrator.Cache;
 
+import android.graphics.Bitmap;
+import android.media.Image;
+import android.util.Log;
+import android.util.LruCache;
+
 import com.renyu.administrator.Bean.ImageBean;
 
 /**
@@ -8,13 +13,24 @@ import com.renyu.administrator.Bean.ImageBean;
  * 注释：从内存中读取
  */
 public class MemoryCacheObservableextends extends CacheObservable{
+    private int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024);
+    private int cacheSize = maxMemory / 4;
+    private LruCache<String, Bitmap> mLruCache = new LruCache<String, Bitmap>(cacheSize){
+        @Override
+        protected int sizeOf(String key, Bitmap value) {
+            //sizeOf方法就是计算每张图片byte大小的方法
+            return value.getRowBytes() * value.getHeight() / 1024;
+        }
+    };
     @Override
     public ImageBean getDataFromCache(String url) {
-        return null;
+        Log.d("renyu", "getDataFromMemoryCache");
+        Bitmap bitmap = mLruCache.get(url);
+        return new ImageBean(bitmap, url);
     }
 
     @Override
     public void putDataToCache(ImageBean imageBean) {
-
+        mLruCache.put(imageBean.getUrl(), imageBean.getBitmap());
     }
 }
--- app/src/main/java/com/renyu/administrator/l3cache/MainActivity.java
+++ app/src/main/java/com/renyu/administrator/l3cache/MainActivity.java
@@ -3,12 +3,19 @@ package com.renyu.administrator.l3cache;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.renyu.administrator.Loader.RxImageLoader;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageView mImageView;
+    String url = "http://img2.baa.bitautotech.com/usergroup/editor_pic/2017/3/22/694494c2f3544226ae911bf86b4e2bcc.png";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mImageView = findViewById(R.id.image);
+        RxImageLoader.with(this).load(url).into(mImageView);
+
     }
 }
